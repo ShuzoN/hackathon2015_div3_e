e14966a035be96396296d1e48d1ffedc16a8cb66
ShuzoN <d.namu.r21@gmail.com>
2015-08-31 03:58:50 +0900

    db/seed.rb | add regions init data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+#
+#=================
+# region 初期化
+#=================
+@region_sect = ["真言", "天台", "日蓮", "浄土", "浄土真"]
+@region_decription= ["真言の説明", "天台の説明", "日蓮の説明", "浄土の説明", "浄土真の説明"]
+
+@region_sect.each_index do |index|
+    Region.create(:sect=>@region_sect[index],:description=>@region_decription[index])
+  end
