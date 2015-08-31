8db01d02988e4a1361b6d06003ca0594887d475f
ShuzoN <d.namu.r21@gmail.com>
2015-08-31 22:51:56 +0900

    create routing modified

--- app/controllers/temples_controller.rb
+++ app/controllers/temples_controller.rb
@@ -33,7 +33,7 @@ class TemplesController < ApplicationController
         #redirect_to は改めてページにアクセス
         #renderはviewファイルを出力して表示するだけ
         format.html { redirect_to @temple }
-        format.json { render action: :show, status: :created, location: @temple }
+        format.json { render :show, status: :created, location: @temple }
       else
         format.html { render :new , layout: :show}
         format.json { render json: @temple.errors, status: :unprocessable_entity }
